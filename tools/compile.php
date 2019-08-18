a56332c4f0b5e0b306142c362ca41081effe31fe
Karl Wilcox <karlwilcox36@gmail.com>
2019-08-18 23:43:23 +0100

    Update images, changes to image generation tool

--- tools/compile.php
+++ tools/compile.php
@@ -7,8 +7,8 @@
 //     $targetDir = "/Users/karlw/gdrive/Projects/drawshield/source/_catalog/";
 //     $sourceDir = "/Users/karlw/gdrive/Projects/draw-code/include/svg/";
 // }
-$targetDir = "/home/karlw/projects/drawshield/source/_catalog/";
-$sourceDir = "/home/karlw/projects/draw-code/include/svg/";
+$targetDir = "/media/sf_gdrive/Projects/drawshield/source/_catalog/";
+$sourceDir = "/media/sf_gdrive/Projects/draw-code/include/svg/";
 
 $tempDir = "/tmp";
 
