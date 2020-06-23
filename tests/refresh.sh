c24f6b0403c6a5fe28bb7a6848adb4670b353176
Karl R. Wilcox <karlwilcox36@gmail.com>
2020-06-23 20:05:55 +0100

    More testcases

--- tests/refresh.sh
+++ tests/refresh.sh
@@ -13,6 +13,11 @@ for i in $myargs; do
   name=${name%.txt}
   args=$(sed -e '/^[[:space:]]*#/d' -e '/^[[:space:]]*$/d' -e 's/ *= */=/' -e 's/.*/--data-urlencode "&"/' $i | paste '-sd ')
   eval curl --silent $args $url$target > "expected/$name.svg"
-  echo Created: $name with $args
+  # echo Created: $name with $args
+  echo Created: $name
+  if grep 'category="blazon"' "expected/$name.svg" > /dev/null
+  then
+    echo 'Warning - error in blazon'
+  fi
   sleep 1
 done
